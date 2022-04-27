573683243e4c850d5035f163461dcfbd06aadf8f
suvesh.p <suvesh.p@media.net>
2022-04-27 23:06:51 +0530

    change: website metadata

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -14,7 +14,7 @@ metaphy                <title style={{fontFamily: 'Zen Dots'}}>Metaphy</title>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <meta name="description" content='The Social Passport to Metaverse' />
                 <meta property="og:title" content="VirtualWide" />
-                <meta property="og:description" content="Play games, party hard & meet your favorite people in life-like virtual worlds" />
+                <meta property="og:description" content="Create and discover interactive metaverses for your brand" />
                 <meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/virtualwide-2426e.appspot.com/o/virtualWide.webp?alt=media&token=f9131f68-8741-4446-907e-66ca95737277" />
             </Head>
             <LayoutContainer background={background} id="layout" onScroll={(event: any) => {
